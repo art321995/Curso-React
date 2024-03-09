1dc1d6d21d4f72006c6c1a78c0b91b98859d740e
Arturo Esqueda <art270112@gmail.com>
2024-03-09 09:36:49 -0600

    Actualizacion lista de Super

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -108,7 +108,7 @@ const App = () =>{
         
       </main>
 
-      <footer className="text-center mt-8 dark:text-gray-400 transition-all duration-1000">Drag and drop to reorder list</footer>
+      <footer className="text-center mt-8 dark:text-gray-400 transition-all duration-1000">Lista de Super</footer>
 
     </div>
   );
